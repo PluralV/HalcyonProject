03fe0bf16cf50e98b70c25aef784b5f6040d79d6
Bennett Schirmer <bennett.schirmer@stonybrook.edu>
2025-11-04 20:19:50 -0500

    Added more variables, functions to Ship class base

--- Source/Halcyon/ShipPawn.h
+++ Source/Halcyon/ShipPawn.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Pawn.h"
+#include "InputActionValue.h"
 #include "ShipPawn.generated.h"
 
 class UInputMappingContext;
@@ -18,6 +19,15 @@ class HALCYON_API AShipPawn : public APawn
 public:
 	// Sets default values for this pawn's properties
 	AShipPawn();
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	USkeletalMeshComponent* ShipMesh;
+
+	/*UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	USpaceshipMovementComponent* MovementComponent;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	USpaceshipThrustComponent* ThrustComponent;*/
 
 protected:
 	// Called when the game starts or when spawned
@@ -42,6 +52,58 @@ protected:
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
 	UInputAction* YawAction;
 
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
+	UInputAction* TargetAction;
+
+	//Handle player input
+	void Look(const FInputActionValue& Value);
+	void Throttle(const FInputActionValue& Value);
+	void Decelerate(const FInputActionValue& Value);
+	void Steer(const FInputActionValue& Value);
+	void ZeroThrottle();
+	void ZeroDecel();
+	void ZeroSteering();
+	void Target(const FInputActionValue& Value);
+
+	// Camera rotation speed
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Camera")
+	float CameraRotationSpeed = 1.f;
+
+	//Ship base stats
+	//Hull integrity
+	int32 HullIntegrity = 32;
+	//Shield values
+	TArray<int32> ShieldFacings = { 30,24,18,18,18,24 };
+	
+	//Hull damage: Internal damage that does nothing
+	int32 CenterHull = 0;
+	int32 ForwardHull = 8;
+	int32 AftHull = 12;
+	
+	//Energy stats: The total amount of energy generated is the total integer sum of these
+	int32 LeftEng = 16;
+	int32 RightEng = 16;
+	int32 CenterEng = 0;
+	int32 PowerReactor = 4;
+
+	//
+
+	//ARRAYS FOR STORING SYSTEMS
+
+
+	//Engine settings
+	UPROPERTY()
+	float SpeedLimit = 1750.f;
+	float CurrentThrottle = 0.f;
+	const float DefaultSpeedLimit = 1750.f;
+	//Maneuverability
+	float PitchRate = 20.f;
+	float YawRate = 20.f;
+	//Top speed, multiplied by amount of energy allocated to movement to get the maximum velocity (magnitude)
+	float SpeedConstant = 20.f;
+	
+	
+
 
 public:	
 	// Called every frame
@@ -49,5 +111,11 @@ public:
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+private:
 
+	//Power allocated to base non-external systems
+	int32 MovementEnergy = 0;
+
+	FVector CurrentVelocity;
+	FRotator AngularVelocity;
 };
